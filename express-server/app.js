300e385c20a01ad2f528fbfe57b8247d19b22a47
starhumang <starhumang@naver.com>
2023-11-27 17:51:17 +0900

    d

--- express-server/app.js
+++ express-server/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const getData = async() => {
+   return await fetch('db.json')
+    .then(res => res.json());
+}
+
+let db = getData();
+
+app.get('/posts', (req, res) => {
+    res.send(db['posts']);
+})
+
 //라우팅 등록(경로, 핸들러[콜백함수]) = 클라이언트의 요청에 애플리케이션이 응답하는 방법을 결정하는 것
 app.get('/', (req, res)=>{
  res.send('Server Connect');
